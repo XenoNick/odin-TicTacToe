1f3f8451a84bc4868959b26d79533b3d7a5b3e2c
Nick <124405252+XenoNick@users.noreply.github.com>
2023-05-29 13:58:41 -0400

    forgot to commit, lots of changes..

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const gameBoard = (() => {
   const board = [];
-  const display = () => [...board];
-  const setUp = () => {
+  const getCurrentMarkers = () => [...board];
+  const reset = (pieces) => {
     for (let i = 0; i < 9; i++) {
-      board[i] = null;
+      board[i] = pieces;
     }
   };
   const update = (index, marker) => {
@@ -17,8 +17,42 @@ const gameBoard = (() => {
     }
     board[index] = marker;
   };
-  setUp();
-  return { display, setUp, update };
+  return { getCurrentMarkers, reset, update };
 })();
 
-const playerCreator = (name, marker) => {};
+const playerCreator = (name, marker) => {
+  const player = { name, marker };
+  const getName = () => player.name;
+  const getMarker = () => player.marker;
+  const setMarker = (newMark) => {
+    if (typeof newMark !== 'string' || newMark.length !== 1) {
+      console.log('Not a string or exceeds required length of 1!');
+      return;
+    }
+    player.marker = newMark.toUpperCase();
+  };
+  return { getName, getMarker, setMarker };
+};
+
+const displayController = (() => {
+  const tiles = [...document.querySelectorAll('.tiles')];
+  const markers = [...document.querySelectorAll('.marker')];
+
+//   tiles.forEach((elm) => {
+//     elm.addEventListener('click', ({target}) => {
+        
+//     });
+//   });
+
+  const resetDisplay = () => {
+    gameBoard.reset(null);
+    markers.forEach((elm, index) => {
+      if (gameBoard.getCurrentMarkers()[index] === null) return 0;
+      elm.textContent = gameBoard.getCurrentMarkers()[index];
+      return 1;
+    });
+  };
+
+  resetDisplay();
+  return { resetDisplay };
+})();
